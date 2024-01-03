0378c3c3f94527153c5ec0f4ecf3411ffb1e6eb6
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2024-01-03 18:45:57 +0200

    typescript #3

--- src/2.ts
+++ src/2.ts
@@ -1,5 +1,5 @@
 /*
-  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
+  Ваше завдання полягатиме у створенні двох класів - Employee та Manager.
 
   Клас Employee повинен включати:
 
@@ -16,11 +16,11 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+ public name: string;
+ private department: string;
+ protected salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor( name: string,  department: string, salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +33,13 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, departament: string, salary: number) {
+    super(name,departament,salary +  10000)
+  }
 }
+
+// const manager = new Manager('Name', '123', 100)
+// console.log(manager);
 
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,50 @@
+class Key  {
+    private signature: number = Math.random(); 
+    public getSignature():number {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    public getKey():Key {
+        return this.key
+    }
+}
+
+
+
+abstract class House{
+    public door: boolean = false;
+    private tenants: Person[] = [];
+
+    constructor(protected key: Key) { }
+    
+    comeIn(tenant: Person):void {
+        if (this.door) {
+            this.tenants.push(tenant) 
+        }
+    }
+    abstract openDoor(key: Key):void
+}
+
+class MyHouse extends House{
+    public openDoor(key: Key):void {
+        if (this.key.getSignature() === key.getSignature()) {
+            console.log("Дверь открыта");
+            
+            this.door = true
+        }
+        this.door = false
+    }
+}
+
+
+
+
 const key = new Key();
+console.log(key.getSignature());
+
 
 const house = new MyHouse(key);
 const person = new Person(key);
